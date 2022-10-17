c368f58b1f96af7463f1805ab2930fe166f08209
Alice52 <zzhang_xz@163.com>
2022-10-17 22:20:52 +0800

    syntax: addunit test note

---
+++ tutorials/cn.edu.ntu.awesome/test/split_test.go
@@ -0,0 +1,92 @@
+package test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func ExampleSplit() {
+	fmt.Println(Split("a:b:c", ":"))
+	fmt.Println(Split("沙河有沙又有河", "沙"))
+	// Output:
+	// [a b c]
+	// [ 河有 又有河]
+}
+
+// 测试集的Setup与Teardown
+func setupTestCase(t *testing.T) func(t *testing.T) {
+	t.Log("如有需要在此执行:测试之前的setup")
+	return func(t *testing.T) {
+		t.Log("如有需要在此执行:测试之后的teardown")
+	}
+}
+
+func BenchmarkSplitParallel(b *testing.B) {
+	// b.SetParallelism(1) // 设置使用的CPU数
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			Split("沙河有沙又有河", "沙")
+		}
+	})
+}
+
+func BenchmarkSplit(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Split("沙河有沙又有河", "沙")
+	}
+}
+
+func TestSplit(t *testing.T) {
+	type test struct { // 定义test结构体
+		input string
+		sep   string
+		want  []string
+	}
+	tests := map[string]test{ // 测试用例使用map存储
+		"simple":      {input: "a:b:c", sep: ":", want: []string{"a", "b", "c"}},
+		"wrong sep":   {input: "a:b:c", sep: ",", want: []string{"a:b:c"}},
+		"more sep":    {input: "abcd", sep: "bc", want: []string{"a", "d"}},
+		"leading sep": {input: "沙河有沙又有河", sep: "沙", want: []string{"", "河有", "又有河"}},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) { // 使用t.Run()执行子测试
+			got := Split(tc.input, tc.sep)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("expected:%#v, got:%#v", tc.want, got)
+			}
+		})
+	}
+}
+
+func TestSplitV1(t *testing.T) {
+	// 定义一个测试用例类型
+	type test struct {
+		input string
+		sep   string
+		want  []string
+	}
+	// 定义一个存储测试用例的切片
+	tests := []test{
+		{input: "a:b:c", sep: ":", want: []string{"a", "b", "c"}},
+		{input: "a:b:c", sep: ",", want: []string{"a:b:c"}},
+		{input: "abcd", sep: "bc", want: []string{"a", "d"}},
+		{input: "沙河有沙又有河", sep: "沙", want: []string{"", "河有", "又有河"}},
+	}
+	// 遍历切片，逐一执行测试用例
+	for _, tc := range tests {
+		got := Split(tc.input, tc.sep)
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("expected:%#v, got:%#v", tc.want, got)
+		}
+	}
+}
+
+func TestSplitV0(t *testing.T) {
+	defer setupTestCase(t)(t)
+	got := Split("a:b:c", ":")
+	expect := []string{"a", "b", "c"}
+	if !reflect.DeepEqual(expect, got) {
+		t.Errorf("expected:%v, got:%v", expect, got)
+	}
+}
